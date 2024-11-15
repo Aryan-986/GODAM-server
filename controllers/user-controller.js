9667013f8ea2927008ee0243522e30b8568be322
Aryan-986 <aryankarki984@gmail.com>
2024-11-15 19:04:11 +0545

    Added user routes and email verification template

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,5 +1,7 @@
+import sendEmail from "../config/sendEmail.js";
 import UserModel from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import verifyEmailTemplate from "../utils/verifyEmailTemplate.js";
 
 export async function registerUserController(request, response) {
   try {
@@ -36,10 +38,22 @@ export async function registerUserController(request, response) {
     const newUser = new UserModel(payload)
     const save = await newUser.save( )
 
+    const verifyEmailUrl = `${process.env.FRONTEND_URL}/verify-email?code${save?._id}`
+
     const verifyEmail = await sendEmail({
           sendTo : email,
           subject : "Verify email from Godam",
-          html :
+          html : verifyEmailTemplate({
+            name,
+            url : verifyEmailUrl
+          })
+    })
+
+    return response.json({
+      message : "user register successfully",
+      error : false,
+      success : true,
+      data : save
     })
 
   } catch (error) {
@@ -47,5 +61,37 @@ export async function registerUserController(request, response) {
       message: error.message || error,
       error: false,
     });
+  }
+}
+
+export async function verifyEmailController(request,response){
+  try {
+      const { code } = request.body 
+
+      const user = await UserModel.findOne({_id : code})
+
+      if(!user){
+        return response.status(400).json({
+          message : "Invalid",
+          error : true,
+          success : false 
+        })
+      }
+
+      const updateUser = await UserModel.updateOne({_id : code},{
+        verify_email : true
+      })
+
+      return response.json({
+        message : "verify email done",
+        success : "true",
+        error : "false"
+      })
+  } catch (error) {
+    return response.status(500).json({
+      message: error.message || error,
+      error : true,
+      success : true 
+    })
   }
 }
--- index.js
+++ index.js
@@ -6,29 +6,34 @@ import cookieParser from 'cookie-parser'
 import morgan from 'morgan'
 import helmet from 'helmet'
 import connectDB from './config/connectDB.js'
+import userRouter from './route/user.route.js'
 
 const app = express()
 app.use(cors({
     credentials : true,
     origin : process.env.FRONTEND_URL
 }))
-app.use(express.json())
-app.use(cookieParser())
-app.use(morgan())
-app.use(helmet({
-    crossOriginResourcePolicy : false
-}))
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan());
+app.use(
+  helmet({
+    crossOriginResourcePolicy: false,
+  }));
 
-const PORT = 8080 || process.env.PORT 
+const PORT = 8080 || process.env.PORT;
 
 app.get("/", (request, response) => {
-    response.json({
-        message: "Server is running on port " + PORT
-    });
+  //server to client
+  response.json({
+    message: "Server is running on port " + PORT,
+  });
 });
 
+app.use('/api/user',userRouter)
+
 connectDB().then(()=>{
-    app.listen(PORT,()=>{
-        console.log("Server is running",PORT)
-    })
-})
+  app.listen(PORT,()=>{
+    console.log("Server is running", PORT);
+  });
+});
