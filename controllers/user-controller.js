6e25ecd75caf0e76d0d86724029410b0565cfccf
Aryan-986 <aryankarki984@gmail.com>
2024-11-16 13:41:31 +0545

    Created Login API and test in postman

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -2,6 +2,8 @@ import sendEmail from "../config/sendEmail.js";
 import UserModel from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import verifyEmailTemplate from "../utils/verifyEmailTemplate.js";
+import generatedAcessToken from "../utils/generateAccessToken.js";
+import generatedRefreshToken from "../utils/generatedRefreshToken.js";
 
 export async function registerUserController(request, response) {
   try {
@@ -30,68 +32,141 @@ export async function registerUserController(request, response) {
     const hashPassword = await bcryptjs.hash(password, salt);
 
     const payload = {
-        name,
-        email,
-        password : hashPassword
-    }
+      name,
+      email,
+      password: hashPassword,
+    };
 
-    const newUser = new UserModel(payload)
-    const save = await newUser.save( )
+    const newUser = new UserModel(payload);
+    const save = await newUser.save();
 
-    const verifyEmailUrl = `${process.env.FRONTEND_URL}/verify-email?code${save?._id}`
+    const verifyEmailUrl = `${process.env.FRONTEND_URL}/verify-email?code=${save?._id}`;
 
     const verifyEmail = await sendEmail({
-          sendTo : email,
-          subject : "Verify email from Godam",
-          html : verifyEmailTemplate({
-            name,
-            url : verifyEmailUrl
-          })
-    })
+      sendTo: email,
+      subject: "Verify email from Godam",
+      html: verifyEmailTemplate({
+        name,
+        url: verifyEmailUrl,
+      }),
+    });
 
     return response.json({
-      message : "user register successfully",
-      error : false,
-      success : true,
-      data : save
-    })
+      message: "user register successfully",
+      error: false,
+      success: true,
+      data: save,
+    });
 
   } catch (error) {
     return response.status(500).json({
       message: error.message || error,
-      error: false,
+      error: true,
+      success : false
     });
   }
 }
 
-export async function verifyEmailController(request,response){
+export async function verifyEmailController(request, response) {
   try {
-      const { code } = request.body 
+    const { code } = request.body;
 
-      const user = await UserModel.findOne({_id : code})
+    const user = await UserModel.findOne({ _id: code });
 
-      if(!user){
-        return response.status(400).json({
-          message : "Invalid",
-          error : true,
-          success : false 
-        })
-      }
+    if (!user) {
+      return response.status(400).json({
+        message: "Invalid",
+        error: true,
+        success: false,
+      });
+    }
 
-      const updateUser = await UserModel.updateOne({_id : code},{
-        verify_email : true
-      })
+    const updateUser = await UserModel.updateOne({ _id: code },{
+        verify_email: true,
+      });
 
-      return response.json({
-        message : "verify email done",
-        success : "true",
-        error : "false"
-      })
+    return response.json({
+      message: "verify email done",
+      success: "true",
+      error: "false",
+    });
   } catch (error) {
     return response.status(500).json({
       message: error.message || error,
-      error : true,
-      success : true 
-    })
+      error: true,
+      success: true,
+    });
   }
 }
+
+//login controller
+export async function loginController(request, response) {
+  try {
+    const { email, password } = request.body;
+
+    if(!email || !password){
+      return response.status(400).json({
+        message : "provide email, password",
+        error : true,
+        success : false
+      })
+    }
+
+    const user = await UserModel.findOne({ email });
+
+    if (!user) {
+      return response.status(400).json({
+        message: "user not registered",
+        error: true,
+        success: false,
+      });
+    }
+
+    if (user.status !== "Active") {
+      return response.status(400).json({
+        message: "contact to Admin",
+        error: true,
+        success: false,
+      });
+    }
+
+    const checkPassword = await bcryptjs.compare(password, user.password);
+
+    if (!checkPassword) {
+      return response.status(400).json({
+        message: "Check your Password",
+        error: true,
+        success: false,
+      });
+    }
+
+    const accesstoken = await generatedAcessToken(user._id)
+    const refreshToken = await generatedRefreshToken(user._id)
+
+    const cookiesOption = {
+      httpOnly : true,
+      secure : true,
+      sameSite : "None"
+    }
+    response.cookie('accessToken',accesstoken, cookiesOption)
+    response.cookie('refreshToken',refreshToken, cookiesOption)
+
+    return response.json({
+      message : "Login successfully",
+      error : false,
+      success : true,
+      data: {
+        accesstoken,     
+        refreshToken                                                                                                                           
+      }
+    })
+
+  } catch (error) {
+    return response.status(500).json({
+      message: error.message || error,
+      success: false,
+    });
+  }
+}
+
+
